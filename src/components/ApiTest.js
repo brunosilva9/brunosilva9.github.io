45e39cfbfa391a2670373f93a2efff9d78731b94
Bsilva <bruno.silva.paredes@gmail.com>
2023-02-20 18:45:49 -0300

    reverseUsememoMultiCall

--- src/components/ApiTest.js
+++ src/components/ApiTest.js
@@ -1,32 +1,35 @@
-import React, { useMemo } from "react";
+import React from "react";
+
 import { useTranslation } from "react-i18next";
 import { useRememberState } from "use-remember-state";
+
 
 const ApiTest = () => {
   const { t } = useTranslation('global');
   const axios = require('axios');
-  const [listPj, setListPj] = useRememberState("localPj",[]);
+  var [listPj, setListPj] = useRememberState("localPj",[]);
+  axios.get('https://swapi.dev/api/people')
+    .then(resp => {
+      console.log(resp.data);
+      setListPj(resp.data.results);
+    })
 
-  useMemo(() => {
-    axios.get('https://swapi.dev/api/people')
-      .then(resp => {
-        console.log(resp.data);
-        setListPj(resp.data.results);
-      });
-  }, [listPj]);
-
-  const characterList = useMemo(() => {
-    return listPj.map(pj => <p>{pj.name}</p>);
-  }, [listPj]);
 
   return (
     <div>
       <div style={{ textAlign: "center", fontWeight: "500" }}>
         {t("testApi.header")}
-        {characterList}
+        {listPj.map((pj) => {
+          return (
+            <p>{pj.name}</p>
+          )
+        })}
       </div>
     </div>
+
   );
+
 };
+
 
 export default ApiTest;
