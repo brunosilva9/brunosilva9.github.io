fafb10ebc3dfe5bcb306400f4aaf3eb1b96c4260
Bsilva <bruno.silva.paredes@gmail.com>
2023-02-21 22:50:51 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcd2aa0d15f93610d29008fec6449275.js"
+  "/precache-manifest.2b5efc5b3a99f9481f2d89d504a94c72.js"
 );
 
 workbox.clientsClaim();
